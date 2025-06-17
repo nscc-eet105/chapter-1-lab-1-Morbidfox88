b43c095ca697c52c74c0af540167d6a46553d636
Chad Collard <chadcollard1567@gmail.com>
2025-06-17 14:31:13 -0400

    chapter 1 lab finished

---
+++ Lab1.py
@@ -0,0 +1,7 @@
+print ("Speak the truth!")
+print ("Speak it boldly, never fear;")
+print ("Speak it so that all may hear;")
+print ("In the end it shall appear")
+print ("Truth is best in age and youth.")
+print ("Speak the truth.")
+print ("Chad Collard")
